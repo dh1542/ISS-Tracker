258c0ca2090beedf415d4ebaa4f8c23bb890a2d9
Dominik <Dominik@mbp>
2022-02-21 08:13:12 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header title="ISS Location Tracker" text="Where to find the International Space Station right now?"/>
+    <Map />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Map from './components/Map.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Map
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/** Import google font Roboto */
+@import url('https://fonts.googleapis.com/css2?family=Roboto&family=Zen+Kaku+Gothic+Antique&display=swap');
+
+html{
+  font-family: roboto;
+  background-color: white;
 }
+
+#header{
+  
+
+  text-align: center;
+  padding: 1%;
+  padding-bottom: 0;
+  margin-bottom: 0;
+  border: 1rem;
+  
+}
+#header h1{
+  margin-bottom: 0;
+  
+}
+
+#header p{
+  font-size: 90%;
+  margin-top: 0;
+  
+}
+
+#header hr{
+  border-top: 1% solid red;
+}
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="header">
+        <h1>{{title}}</h1>
+        <p>{{text}}</p>
+        <hr>
+    </div>
+    
+</template>
+<script>
+export default {
+    props:{
+        title: String,
+        text: String
+    }
+    
+}
+</script>
+
+<style scoped>
+hr{
+    color: red;
+    
+}
+</style>
